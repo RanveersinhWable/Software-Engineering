53cd6974208a1cb0af6b2c7929d20e654305e989
kirtiyes <156543668+kirtiyes@users.noreply.github.com>
2024-11-22 13:29:29 +0530

    Delete RecyclingPlant/frontend/src/pages directory

--- RecyclingPlant/frontend/src/pages/RemoveDepartment.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import AdminDepartments from './AdminDepartments'; // Reuse AdminDepartments for functionality
-
-const RemoveDepartment = () => {
-    return <AdminDepartments />; // Use the existing AdminDepartments component
-};
-
-export default RemoveDepartment;
